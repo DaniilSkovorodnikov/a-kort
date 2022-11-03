4afa5b99fcea62a941531f9bdcf5eaff64a714b1
DaniilSkovorodnikov <95247591+DaniilSkovorodnikov@users.noreply.github.com>
2022-11-03 14:33:32 +0500

    Add a photo's

--- a-kort/src/components/MenuChangerComponents/Dish.js
+++ a-kort/src/components/MenuChangerComponents/Dish.js
@@ -3,7 +3,7 @@ export default function Dish({name, price, photo}){
       <div className="dish">
           <img src={photo} width={245} height={160} alt="" className="dish__photo"/>
           <h2 className="dish__name">{name}</h2>
-          <p className="dish__price">{price} ₽</p>
+          <p className="dish__price">{price} <span>&#8381;</span></p>
       </div>
     );
 }
