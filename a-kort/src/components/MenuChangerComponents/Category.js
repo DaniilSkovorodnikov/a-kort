33e0a975f275a7bb7b84f64d23c14cdb599730ef
DaniilSkovorodnikov <95247591+DaniilSkovorodnikov@users.noreply.github.com>
2022-11-07 16:38:50 +0500

    Fix architecture

--- a-kort/src/components/MenuChangerComponents/Category.js
+++ a-kort/src/components/MenuChangerComponents/Category.js
@@ -2,43 +2,130 @@ import Dish from "./Dish";
 import Modal from "./Modal";
 import {useState} from "react";
 
-export default function Category({name, setVisible, id ,setCurrentCategory, dishes}){
+export default function Category({name, id, dishes}){
     const index = id
+    const categoryName = name
+
     const [visibleChanger, setVisibleChanger] = useState(false);
+    const [visibleDishAdder, setVisibleDishAdder] = useState(false);
+
+    const [dishPrice, setPrice] = useState('');
+    const [dishName, setDishName] = useState('');
+    const [dishDesc, setDesc] = useState('');
+    const [dishPhoto, setPhoto] = useState();
+    const [photoURL, setPhotoURL] = useState()
+
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+        setPhotoURL(fileReader.result)
+    }
+
+    const [categoryDishes, setCategoryDishes] = useState(dishes)
+
+    function AddNewDish(dish_name, dish_price, dish_description, dish_image){
+        const dish = {
+            dish_name,
+            dish_price,
+            dish_description,
+            dish_image,
+            dish_category: categoryName
+        }
+        fetch("http://127.0.0.1:8000/add_dish/", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify({dish})
+        })
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err))
+        setCategoryDishes([...categoryDishes, dish])
+    }
 
     return(
         <div>
             <Modal visible={visibleChanger} setVisible={setVisibleChanger}>
                 <ul className="current-dishes">
-                    {dishes.map((v, i) => <li className="current-dishes__item" key={i}>
+                    {categoryDishes.map((v, i) => <li className="current-dishes__item" key={i}>
                         <p>{i + 1}. {v.dish_name}</p>
                         <button onClick={() => {
-                            const dishIndex = i;
-                            dishes.splice(dishIndex, 1);
-                            console.log(dishes);
+                            setCategoryDishes((prevState) => prevState.filter((dish) => {
+                                return dish.dish_name !== v.dish_name
+                            }))
                         }}>Удалить</button>
                     </li>)}
                 </ul>
+            </Modal>
+            <Modal visible={visibleDishAdder} setVisible={setVisibleDishAdder}>
+                <div className="dish-adder">
+                    <div className="dish-characteristics" onClick={(e) => e.stopPropagation()}>
+                        <div className="dish-characteristics__left">
+                            <input
+                                className="dish-characteristics__photo"
+                                type="file"
+                                onChange={(e) => {
+                                    setPhoto(e.target.files[0]);
+                                    fileReader.readAsDataURL(e.target.files[0])
+                                }}
+                            />
+                            <div className="dish-characteristics__numerical">
+                                <input
+                                    className="dish-characteristics__price"
+                                    placeholder="Цена"
+                                    value={dishPrice}
+                                    onChange={(e) => setPrice(e.target.value)}
+                                />
+                                <button
+                                    className="dish-characteristics__add-btn"
+                                    onClick={() => {
+                                        setVisibleDishAdder(false);
+                                        AddNewDish(dishName, dishPrice, dishDesc, dishPhoto)
+                                        setPrice('')
+                                        setDishName('')
+                                        setDesc('')
+                                    }
+                                    }
+                                >
+                                    Добавить блюдо
+                                </button>
+                            </div>
+                        </div>
+                        <div className="dish-characteristics__right">
+                            <input
+                                className="dish-characteristics__name"
+                                placeholder="Название"
+                                value={dishName}
+                                onChange={(e) => setDishName(e.target.value)}
+                            />
+                            <input
+                                className="dish-characteristics__desc"
+                                placeholder="Описание"
+                                value={dishDesc}
+                                onChange={(e) => setDesc(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                </div>
             </Modal>
             <div className="category">
                 <div className="category__header">
                     <h2 className="category__name">{name}</h2>
                     <div className="category__buttons">
-                        <button onClick={() => setVisibleChanger(true)}>Изменить категорию</button>
                         <button onClick={() => {
-                            setVisible(true)
-                            setCurrentCategory(index)
-                        }} className="category__add-btn">Добавить блюдо</button>
+                            console.log(categoryDishes);
+                            setVisibleChanger(true)
+                        }}>Изменить категорию</button>
+                        <button onClick={() => setVisibleDishAdder(true)} className="category__add-btn">Добавить блюдо</button>
                     </div>
                 </div>
                 <ul className="category__dishes">
-                    {dishes.map((v, i) => {
-                        console.log(dishes);
+                    {categoryDishes.map((v, i) => {
                         return <Dish name={v.dish_name} price={v.dish_price} photo={v.dish_image} key={i}/>;
                     })}
                 </ul>
             </div>
         </div>
-
-    )
+    );
 }
--- a-kort/src/components/MenuChangerComponents/MenuChanger.js
+++ a-kort/src/components/MenuChangerComponents/MenuChanger.js
@@ -24,51 +24,17 @@ export default function MenuChanger(){
     useEffect(() => {
         const setData = (categories, dishes) =>{
             setCategories(categories);
-            setCategoryDishes([...dishes])
+            setInitialCategoryDishes([...dishes])
         }
         getData().then((v) => setData(v[0], v[1]))
     }, [])
 
-
     const [visibleCategoryAdder, setVisibleCategoryAdder] = useState(false);
-    const [visibleDishAdder, setVisibleDishAdder] = useState(false);
 
     const [categoryName, setCategoryName] = useState("");
-    const [currentCategory, setCurrentCategory] = useState(-1);
-    const [photo, setPhoto] = useState();
-    const [photoURL, setPhotoURL] = useState()
-    const fileReader = new FileReader();
-    fileReader.onloadend = () => {
-        setPhotoURL(fileReader.result)
-    }
-    const [price, setPrice] = useState('');
-    const [name, setDishName] = useState('');
-    const [desc, setDesc] = useState('');
-    const [categoryDishes, setCategoryDishes] = useState([]);
     const [categories, setCategories] = useState([]);
 
-    function AddNewDish(name, price, description, photo){
-        const dish = {
-            dish_name: name,
-            dish_price: price,
-            dish_description: description,
-            dish_image: photo,
-            dish_category: categories[currentCategory]
-        }
-        fetch("http://127.0.0.1:8000/add_dish/", {
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            method: "POST",
-            body: JSON.stringify({dish})
-        })
-            .then((res) => res.json())
-            .then((res) => console.log(res))
-            .catch((err) => console.log(err))
-        categoryDishes[currentCategory].push((dish));
-    }
-
+    const [initialCategoryDishes, setInitialCategoryDishes] = useState([]);
 
     return (
         <div className="menu-changer">
@@ -81,73 +47,18 @@ export default function MenuChanger(){
                     />
                     <button onClick={() => {
                         setCategories([...categories, categoryName])
-                        setCategoryDishes([...categoryDishes, []])
                         setVisibleCategoryAdder(false);
                         setCategoryName('')
                     }} className="category-adder__add-btn">Добавить категорию</button>
                 </div>
             </Modal>
-            <Modal visible={visibleDishAdder} setVisible={setVisibleDishAdder}>
-                <div className="dish-adder">
-                    <div className="dish-characteristics" onClick={(e) => e.stopPropagation()}>
-                        <div className="dish-characteristics__left">
-                            <input
-                                className="dish-characteristics__photo"
-                                type="file"
-                                onChange={(e) => {
-                                    setPhoto(e.target.files[0]);
-                                    fileReader.readAsDataURL(e.target.files[0])
-                                }}
-                            />
-                            <div className="dish-characteristics__numerical">
-                                <input
-                                    className="dish-characteristics__price"
-                                    placeholder="Цена"
-                                    value={price}
-                                    onChange={(e) => setPrice(e.target.value)}
-                                />
-                                <button
-                                    className="dish-characteristics__add-btn"
-                                    onClick={() => {
-                                        setVisibleDishAdder(false);
-                                        AddNewDish(name, price, desc, photoURL);
-                                        setPrice('')
-                                        setDishName('')
-                                        setDesc('')
-                                    }
-                                }
-                                >
-                                    Добавить блюдо
-                                </button>
-                            </div>
-                        </div>
-                        <div className="dish-characteristics__right">
-                            <input
-                                className="dish-characteristics__name"
-                                placeholder="Название"
-                                value={name}
-                                onChange={(e) => setDishName(e.target.value)}
-                            />
-                            <input
-                                className="dish-characteristics__desc"
-                                placeholder="Описание"
-                                value={desc}
-                                onChange={(e) => setDesc(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </Modal>
             <MenuHeader setVisible={setVisibleCategoryAdder} />
             <div className="menu">
                 {categories.map((v, index ) => {
-                    console.log(categoryDishes[index])
                     return <Category name={v}
-                              id={index}
-                              key={index}
-                              setVisible={setVisibleDishAdder}
-                              setCurrentCategory={setCurrentCategory}
-                              dishes={categoryDishes[index]}
+                                     id={index}
+                                     key={index}
+                                     dishes={initialCategoryDishes[index] === undefined ? [] : initialCategoryDishes[index]}
                     />
                 })}
             </div>
