1640196c59da6cb076bd93376049598f393ba968
DaniilSkovorodnikov <95247591+DaniilSkovorodnikov@users.noreply.github.com>
2022-11-10 14:48:24 +0500

    Fix add dishes in user menu

--- a-kort/src/components/App.js
+++ a-kort/src/components/App.js
@@ -1,6 +1,7 @@
 import '../styles/App.scss';
 import MenuChanger from "./MenuChangerComponents/MenuChanger";
 import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
 import Menu from "./Menu/Menu";
 
 export default function App() {
--- a-kort/src/components/Menu/Menu.js
+++ a-kort/src/components/Menu/Menu.js
@@ -7,33 +7,21 @@ import {useEffect, useState} from "react";
 export default function Menu(){
     useEffect(() => {
         const setData = (categories, dishes) =>{
-            for (let category of dishes){
-                for (let dish of category){
-                    setAllDishes([...allDishes, dish])
-                }
-            }
             setCategories(categories);
             setDishes(dishes)
         }
         getData().then((v) => setData(v[0], v[1]))
     }, [])
 
-    const [allDishes, setAllDishes] = useState([])
     const [dishes, setDishes] = useState([])
     const [categories, setCategories] = useState([])
-    const [currentCategory, setCurrentCategory] = useState(-1)
+    const [currentCategory, setCurrentCategory] = useState(0)
 
     return (
         <div>
             <UserHeader/>
             <div className="user-menu">
                 <ul className="categories">
-                    <li className="categories__item">
-                        <button className="categories__btn" onClick={() => {
-                            setCurrentCategory(-1)
-                        }}>Всё
-                        </button>
-                    </li>
                     {categories.map((v, index) => <li className="categories__item" key={index}>
                         <button className="categories__btn"
                                                           onClick={() => {
@@ -45,8 +33,6 @@ export default function Menu(){
                 </ul>
                 <ul className="dishes">
                         {
-                            currentCategory === -1 ?
-                            allDishes.map((v) => <Dish name={v.dish_name} price={v.dish_price} photo={v.dish_image}/>):
                             dishes[currentCategory].map((v) => <Dish name={v.dish_name} price={v.dish_price} photo={v.dish_image}/>)
                         }
                 </ul>
