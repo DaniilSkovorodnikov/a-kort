2ca00f8e903a0c265a733ea7b4b83ec7da0a2981
DaniilSkovorodnikov <95247591+DaniilSkovorodnikov@users.noreply.github.com>
2022-11-10 14:26:03 +0500

    Fix add dishes in user menu

--- a-kort/src/components/Menu/Menu.js
+++ a-kort/src/components/Menu/Menu.js
@@ -7,12 +7,18 @@ import {useEffect, useState} from "react";
 export default function Menu(){
     useEffect(() => {
         const setData = (categories, dishes) =>{
+            for (const category in dishes){
+                for (const dish in category){
+                    setAllDishes([...allDishes, dish])
+                }
+            }
             setCategories(categories);
             setDishes(dishes)
         }
         getData().then((v) => setData(v[0], v[1]))
     }, [])
 
+    const [allDishes, setAllDishes] = useState([])
     const [dishes, setDishes] = useState([])
     const [categories, setCategories] = useState([])
     const [currentCategory, setCurrentCategory] = useState(-1)
@@ -28,7 +34,7 @@ export default function Menu(){
                         }}>Всё
                         </button>
                     </li>
-                    {categories.map((v, index) => <li className="categories__item">
+                    {categories.map((v, index) => <li className="categories__item" key={index}>
                         <button className="categories__btn"
                                                           onClick={() => {
                                                               setCurrentCategory(index)
@@ -40,7 +46,7 @@ export default function Menu(){
                 <ul className="dishes">
                         {
                             currentCategory === -1 ?
-                            dishes.map((v) => {v.map((dish) => <Dish name={dish.dish_name} price={dish.dish_price} photo={dish.dish_image}/>)}):
+                            allDishes.map((v) => <Dish name={v.dish_name} price={v.dish_price} photo={v.dish_image}/>):
                             dishes[currentCategory].map((v) => <Dish name={v.dish_name} price={v.dish_price} photo={v.dish_image}/>)
                         }
                 </ul>
