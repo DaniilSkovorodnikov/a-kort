1bb71fa586692c592519bee272c708674f8886f9
DaniilSkovorodnikov <95247591+DaniilSkovorodnikov@users.noreply.github.com>
2022-11-10 14:14:41 +0500

    Fix user menu

--- a-kort/src/components/Menu/Dish.js
+++ a-kort/src/components/Menu/Dish.js
@@ -3,7 +3,7 @@
 export default function Dish({name, price, photo}){
     return (
         <li className="user-dish">
-            <img className="user-dish__photo" alt="">{photo}</img>
+            <img src={photo} className="user-dish__photo" alt="">{photo}</img>
             <h2 className="user-dish__name">{name}</h2>
             <p className="user-dish__price">{price}</p>
         </li>
