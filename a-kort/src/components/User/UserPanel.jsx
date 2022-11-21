d9b5b494961280c82a2a21ced3ed5a130407d550
DaniilSkovorodnikov <95247591+DaniilSkovorodnikov@users.noreply.github.com>
2022-11-21 18:25:41 +0500

    Fix restaurant styles

--- a-kort/src/components/User/UserPanel.jsx
+++ a-kort/src/components/User/UserPanel.jsx
@@ -33,7 +33,7 @@ export default function UserPanel(){
         </UserHeader>
 
         <ul className="restaurants">
-            {restaurants.map((v,i) => <UserRestaurant name={v.name}
+            {restaurants.map((v) => <UserRestaurant name={v.name}
                                                       rating={v.rating}
                                                       photo={`http://127.0.0.1:8000/get_image/?image=${v.img}`}
             />)}
